2b2b5d9fae78dab6d526a57c3ce6bdfe87b21c46
ineinfomendoza <ineinfomendoza@gmail.com>
2026-05-26 16:23:46 -0300

    agregamos backend node

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "API App Neri funcionando 🚀",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
