a374ebfae3f9745d6134ee0f82ebe4fd40d275b4
ineinfomendoza <ineinfomendoza@gmail.com>
2026-05-26 17:13:17 -0300

    connect express to railway postgres

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 
 const app = express();
 
@@ -13,6 +14,41 @@ app.get("/", (req, res) => {
     status: "ok",
     message: "API App Neri funcionando 🚀",
   });
+});
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+      status: "ok",
+      time: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      status: "error",
+      error: JSON.stringify(error),
+    });
+  }
+});
+
+/* NUEVO ENDPOINT */
+
+
+    res.json({
+      status: "ok",
+      message: "Tabla users creada",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
